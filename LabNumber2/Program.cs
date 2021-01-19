020b9b1fec277b93132a6552c485306d85c011c4
antonio <antoniomanzari@gmail.com>
2021-01-19 14:49:07 -0500

    added welcome message and area calc method

--- LabNumber2/Program.cs
+++ LabNumber2/Program.cs
@@ -4,9 +4,31 @@ namespace LabNumber2
 {
     class Program
     {
+        // Lab 2: Room Calculator
+        // Antonio Manzari
+
+        // Task: Calculate the perimeter and area of various classrooms at Grand Circus
+        // Assume user will input valid numbers, and in a decimal format.
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            // Main Sequence 
+            PrintWelcomeMessage();
+            Console.WriteLine(CalculateArea(2.5,2));
+
+
+            void PrintWelcomeMessage()
+            {
+                Console.WriteLine("Welcome to the Grand Circus Room Detail Generator!");
+                Console.WriteLine("\n"); // Comsemetic padding
+            }
+
+            double CalculateArea(double length, double width)
+            {
+                return length * width;
+            }
+
         }
     }
 }
