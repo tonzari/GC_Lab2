5a87bf899a45978ccbe27e0b40668e71b5d334ee
antonio <antoniomanzari@gmail.com>
2021-01-19 15:40:49 -0500

    fully functional

--- LabNumber2/Program.cs
+++ LabNumber2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 
 namespace LabNumber2
 {
@@ -11,22 +12,77 @@ namespace LabNumber2
         // Assume user will input valid numbers, and in a decimal format.
         static void Main(string[] args)
         {
+            // init
+            double userLength;
+            double userWidth;
+            bool userWantsToContinue = false;
 
 
             // Main Sequence 
             PrintWelcomeMessage();
-            Console.WriteLine(CalculateArea(2.5,2));
+            do
+            {
+                GetUserDimensions();
+                PrintCalculations(CalculateArea(userLength, userWidth), CalculatePerimeter(userLength, userWidth));
+                CheckUserWantsToContinue();
+            }
+            while (userWantsToContinue);
 
 
             void PrintWelcomeMessage()
             {
                 Console.WriteLine("Welcome to the Grand Circus Room Detail Generator!");
+                
+                Console.WriteLine("\n"); // Comsemetic padding
+            }
+
+            void GetUserDimensions()
+            {
+                Console.Write("Enter Length: ");
+                userLength = double.Parse(Console.ReadLine());
+                Console.Write("Enter Width: ");
+                userWidth = double.Parse(Console.ReadLine());
+                
                 Console.WriteLine("\n"); // Comsemetic padding
             }
 
             double CalculateArea(double length, double width)
             {
                 return length * width;
+            }
+
+            double CalculatePerimeter(double length, double width)
+            {
+                return length * 2 + width * 2;
+            }
+
+            void PrintCalculations(double area, double perimeter)
+            {
+                Console.WriteLine("Area: " + area);
+                Console.WriteLine("Perimeter: " + perimeter);
+            }
+
+            void CheckUserWantsToContinue()
+            {
+
+                Console.Write("Continue? (y/n) ");
+                char userInput = Console.ReadKey().KeyChar;
+
+                if (userInput.Equals('y'))
+                {
+                    userWantsToContinue = true;
+                }
+                else if (userInput.Equals('n'))
+                {
+                    userWantsToContinue = false;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter: y or n");
+                    CheckUserWantsToContinue();
+                }
+
+                Console.WriteLine("\n"); // Comsemetic padding
             }
 
         }
